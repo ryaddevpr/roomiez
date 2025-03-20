b9646143804bde00252760fa63d4f99d08587d5f
ryaddevpr <ryaddevpro@gmail.com>
2025-03-20 16:51:49 +0000

    asdasw

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { config } from 'dotenv'; // Load .env file before accessing process.env
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{cors:false});
-  // app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     whitelist: true,
-  //     forbidNonWhitelisted: true,
-  //     groups: ['create'],
-  //     always: true,
-  //   }),
-  // );
+  const app = await NestFactory.create(AppModule);
   
+  // Enable CORS for all origins
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: false,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
